9ea308af52b4aca9cc770960f7dde803f1843b96
Udo von Eynern <udo@voneynern.de>
2022-06-16 17:04:59 +0200

    Initial Commit

---
+++ AppleRepositories/AppleRepositoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleRepositoriesApp.swift
+//  AppleRepositories
+//
+//  Created by Udo Von Eynern on 16.06.22.
+//
+
+import SwiftUI
+
+@main
+struct AppleRepositoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
